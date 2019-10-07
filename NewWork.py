9461064e42d460a0148f2f246a58e999f0bf67a6
IzzieFedynich <54276173+IzzieFedynich@users.noreply.github.com>
2019-10-07 08:45:47 -0500

    Update NewWork.py

--- NewWork.py
+++ NewWork.py
@@ -2,18 +2,25 @@ x = "There are %d tupes of people." % 10
 binary = "binary"
 doNot = "don't"
 y = "Those who know %s and those who %s." % (binary, doNot)
+#plugging in 2 strings into 2 types of people
 
 print(x)
 print(y)
+#printing out "there are 10 types of people"
 
 print("I said: %r" % x)
+#plugging in a string into this joke
 print("I also said: '%s'." % y)
+#plugging in a string into this joke
 
 hilarious = False
 joke_evaluation = "Isn't that joke so funny?! %r"
 print(joke_evaluation % hilarious)
+#Evaluation and 2 strings for the joke above
 
 w = "This is the left side of..."
 e = "a string with a right side."
+#w and e strings for this joke down here
 
 print(w + e)
+#left and right side of a string
