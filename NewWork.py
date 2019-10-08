0386fc6278a4d052e1279034e3f1459274805937
IzzieFedynich <54276173+IzzieFedynich@users.noreply.github.com>
2019-10-08 08:52:09 -0500

    Update NewWork.py

--- NewWork.py
+++ NewWork.py
@@ -31,10 +31,10 @@ print(w + e)
 print("Mary had a little lamb.")
 print("Its fleece was white as %s." % 'snow')
 print("And everywhere that Mary went.")
-# A string being multiplied by a number and lot of periods?
+# A string being multiplied by a number and lot of periods
 print("." * 10)
 
-# CheeseBurger spelt out with a string
+# CheeseBurger spelt out with a string 
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -48,7 +48,7 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-# CheeseBurger
+# CheeseBurger spelt out right now letter by letter
 print(end1 + end2 + end3 + end4 + end5 + end6 + end7 + end8 + end9 + end10 + end11 + end12)
 
 # More Formatting
